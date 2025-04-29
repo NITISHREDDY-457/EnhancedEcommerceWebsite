6cbca275d054adf4c74cfe39c1d53fc17796812b
Uyyala Nitish Reddy <103503915+NITISHREDDY-457@users.noreply.github.com>
2025-04-29 15:59:26 +0530

    Update wsgi.py

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -14,6 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
 application = get_wsgi_application()
-gunicorn myproject.wsgi:application
+gunicorn ecommerce.wsgi:application
 
 
